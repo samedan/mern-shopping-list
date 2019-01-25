79e46c34c10f8fb06a9ae54cf61f7f26a741ab25
Popescu Daniel <same.dan@gmail.com>
2019-01-25 15:18:56 +0100

    Second Commit

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import AppNavbar from './components/AppNavbar';
+import ShoppingList from './components/ShoppingList';
+import { Provider } from 'react-redux';
+import store from './store';
+import ItemModal from './components/ItemModal';
+import {  Container } from 'reactstrap';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+      <div className="App">
+        <AppNavbar></AppNavbar>
+        <Container>
+          <ItemModal />
+          <ShoppingList></ShoppingList>
+        </Container>
+      </div>
+      </Provider>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/components/ShoppingList.js
@@ -0,0 +1,55 @@
+import React, {Component} from 'react';
+import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
+import { CSSTransition, TransitionGroup} from 'react-transition-group';
+import {connect} from 'react-redux';
+import {  getItems, deleteItem } from '../actions/itemActions';
+import PropTypes from 'prop-types';
+
+class ShoppingList extends Component {
+
+    componentDidMount() {
+        this.props.getItems();
+    }
+
+    onDeleteClick = (id) => {
+        this.props.deleteItem(id);
+    }
+
+    render() {
+        
+        const { items } = this.props.item; // item is the whole stuff in teh reducer
+        return (
+            <Container>
+               
+
+                <ListGroup>
+                    <TransitionGroup className="shopping-list">
+                        {items.map(({_id, name})=> (
+                            <CSSTransition key={_id} timeout={500} classNames="fade">
+                            <ListGroupItem>
+                                <Button className="remove-btn"
+                                    color="danger"
+                                    size="sm"
+                                    onClick={this.onDeleteClick.bind(this, _id)}>&times;</Button>
+                                {name}
+                            </ListGroupItem>
+                            </CSSTransition>
+                        ))}
+                    </TransitionGroup>
+                </ListGroup>
+            </Container>
+        );
+    }
+}
+
+ShoppingList.propTypes = {
+    getItems: PropTypes.func.isRequired,
+    deleteItem: PropTypes.func.isRequired,
+    item: PropTypes.object.isRequired
+}
+
+const mapsStateToProps = (state) => ({
+    item: state.item // item in the reducer
+});
+
+export default connect(mapsStateToProps, { getItems, deleteItem })(ShoppingList);
